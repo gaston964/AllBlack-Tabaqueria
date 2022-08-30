71081a1ab6cadcbfa71b089e569e7e659ab4f142
Gaston Gutierrez <gastongutierrez964@gmail.com>
2022-08-30 20:25:40 -0300

    Maquetando la pagina

---
+++ Clase 1/index.js
@@ -0,0 +1,19 @@
+/* let numeroUno = parseInt(prompt("Ingrese un numero ")) ;
+let numeroDos = parseInt(prompt("Ingrese otro")) ;
+let suma = (numeroUno + numeroDos);
+let salida = "El resultado de la suma de los numeros es " + suma;
+alert(salida);
+
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+let edad = parseInt(prompt("Ingrese su edad"))
+
+alert(`Su nombre es ${nombre} ${apellido} y tiene ${edad} años`)
+ */
+/* let numero = parseInt(prompt("Ingrese un numero"));
+if (numero % 2 == 0 ) {
+    alert("Es numero par");
+}
+else{
+    alert("No es par")
+}; */

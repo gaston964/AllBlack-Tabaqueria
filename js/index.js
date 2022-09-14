d9de989ccf2d38c3f4193604d32709da6817664f
Gaston Gutierrez <gastongutierrez964@gmail.com>
2022-09-14 15:18:05 -0300

    Practicando for

--- js/index.js
+++ js/index.js
@@ -186,3 +186,20 @@ let divisas = () =>{
 
 divisas();
 
+/* for(let i = 1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("Fizz Bazz");
+    }
+    else if (i % 7 === 0){
+        console.log("Wuff");
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+} */

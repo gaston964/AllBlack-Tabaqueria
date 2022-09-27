c5cb5dc261c1a2809f2796ab84bf08f026cdc0f1
gaston964 <gastongutierrez964@gmail.com>
2022-09-27 16:47:23 -0300

    Segunda Pre entrega EVENTOS

--- js/index.js
+++ js/index.js
@@ -43,28 +43,74 @@ const total = compra.reduce((acc, item) => acc + item.precio, 0)
 alert(`El total de los productos agregados es ${total} pesos`)
  */
 
-let contenedor = document.getElementById("contenedor");
+/* let contenedor = document.getElementById("contenedor");
 const papeles = [
     { id: 1, nombre: "Papel de Celulosa Sativa Club Tradicional", precio: 350, img: "../resources/Papel-Celulosa-Sativa-Club-tradicional.png" },
     { id: 2, nombre: "Papel OCB Blanco", precio: 220, img: "../resources/Papel-OCB-Blanco.png" },
     { id: 3, nombre: "Papel OCB Organico", precio: 290, img: "../resources/Papel-OCB-organico.png" },
     { id: 4, nombre: "Papel OCB Premium Negro", precio: 250, img: "../resources/Papel-OCB-Premium-Negro.jpg" },
     { id: 5, nombre: "Papel OCB Ultimate", precio: 275, img: "../resources/Papel-OCB-ultimate_1.png" },
-];
+]; */
+let contenedor = document.getElementById("contenedor");
+let carritoCompra = document.getElementById("carritoCompra")
+let botonBuscar = document.getElementById("botonBuscar")
+let inputBuscar = document.getElementById("inputBuscar")
+class Producto {
+    constructor(id, nombre, precio, img) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+    }
+}
+const papeles = []
+papeles.push(new Producto(1, "Papel de Celulosa Sativa Club Tradicional", 250, "../resources/Papel-Celulosa-Sativa-Club-tradicional.png"))
+papeles.push(new Producto(2, "Papel OCB Blanco", 220, "../resources/Papel-OCB-Blanco.png"))
+papeles.push(new Producto(3, "Papel OCB Organico", 292, "../resources/Papel-OCB-organico.png"))
+papeles.push(new Producto(4, "Papel OCB Premium Negro", 250, "../resources/Papel-OCB-Premium-Negro.jpg"))
+papeles.push(new Producto(5, "Papel OCB Ultimate", 275, "../resources/Papel-OCB-ultimate_1.png"))
+console.log(papeles);
+
 papeles.forEach(producto => {
     let item = document.createElement("div");
-    item.className = "div"
+    item.className = "col-xs-12  my-3"
     item.innerHTML = `
-        <h2>Id: ${producto.id}</h2>
-        <h3>Producto: ${producto.nombre}</h3>
-        <h4>$${producto.precio}</h4>
+        <div class="container-card">
+            <img src="${producto.img}" alt="Avatar" class="image img__index" style="width:100%">
+            <h4 class="card-titulo">${producto.nombre}</h4>
+            <h4 class="card-titulo">$${producto.precio}</h4>
+            <div class="middle">
+                <button id = "${producto.id}" class="text">Comprar</button>
+            </div>
+        </div>
     `;
     contenedor.append(item)
+    const boton = document.getElementById(producto.id)
+    boton.addEventListener("click", () => comprarProd(producto))
 });
+const carrito = [];
+const comprarProd = (producto) => {
+    let productoExiste = carrito.find(item => item.id === producto.id)
+    if (productoExiste === undefined) {
+        carrito.push({
+            id: producto.id,
+            nombre: producto.nombre,
+            precio: producto.precio,
 
-/* function respuesta(nombre){
-    console.log("Click", nombre);
+            cantidad: 1
+        })
+    } else {
+        productoExiste.precio += productoExiste.precio;
+        productoExiste.cantidad = productoExiste.cantidad + 1;
+    }
+};
+carritoCompra.addEventListener("click", () => console.log(carrito))
+
+const buscarProducto = (entrada) => {
+    console.log(entrada);
+    let productoBuscado = papeles.find(producto => producto.nombre.includes(entrada))
+    console.log(productoBuscado);
+    inputBuscar.value = ""
 }
-let boton = document.getElementById("boton");
-  // boton.onclick = respuesta;
-boton.addEventListener("click", () => respuesta("gaston")); */
+
+botonBuscar.addEventListener("click", () => buscarProducto(inputBuscar.value))

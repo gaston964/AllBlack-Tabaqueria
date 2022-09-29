4195445974a30c8908f1817f79ae73e01c1c1f02
gaston964 <gastongutierrez964@gmail.com>
2022-09-29 14:58:16 -0300

    Index JS

--- js/index.js
+++ js/index.js
@@ -1,58 +1,29 @@
-/* const productos = [
-    { nombre: "Tabaco Achalay", precio: 250 },
-    { nombre: "Tabaco Achalay Vainilla", precio: 320 },
-    { nombre: "Tabaco Achalay Menta", precio: 280 },
-    { nombre: "Tabaco Las Hojas", precio: 250 },
-    { nombre: "Tabaco Las Hojas Natural", precio: 350 },
+let contenedor = document.getElementById("contenedor");
+const productosIndex = [
+    {id: 1, nombre: "Papel OCB Premium Negro", precio: 150, img: "./resources/Papel-OCB-Premium-Negro.jpg" },
+    {id: 2, nombre: "Tabaco Achalay Vainilla", precio: 320, img: "./resources/tabaco-achalay-vainilla.jpg" },
+    {id: 3, nombre: "Tabaco Cuatro Leguas", precio: 280, img: "./resources/tabaco-cuatro-leguas-50gr.jpg" },
+    {id: 4, nombre: "Filtros Libella Slim Mint", precio: 200, img: "./resources/Filtros-Libella-slim-mint.jpg" },
 ]
-const compra = []
-alert("Bienvenidos a Tabaqueria AllBlack! \n Nuestros productos son:")
-let mensaje = ""
-productos.forEach(item => {
-    mensaje += `${item.nombre} ${item.precio}, \n`;
+productosIndex.forEach(item => {
+    let producto = document.createElement("div");
+    producto.className = "container-card my-3";
+    producto.innerHTML = `
+        <img src="${item.img}" alt="Avatar" class="image img__index"
+            style="width:100%">
+        <h4 class="card-titulo text-center">${item.nombre}</h4>
+        <h4 class="card-titulo">${item.precio}</h4>
+        <div class="middle">
+            <button id="${item.id}" class="text">Comprar</button>
+        </div>
+    `
+    contenedor.append(producto)
 });
-alert(mensaje);
-let salida = "S"
-while (salida === "S") {
-    alert("Que producto desea agregar al carrito?")
-    let opcion = Number(prompt("1- Tabaco Achalay \n 2- Tabaco Achalay Vainilla \n 3- Tabaco Achalay Menta \n 4- Tabaco Las Hojas \n 5- Tabaco Las Hojas Natural"))
-    switch (opcion) {
-        case 1:
-            compra.push(productos[0])
-            break;
-        case 2:
-            compra.push(productos[1])
-            break;
-        case 3:
-            compra.push(productos[2])
-            break;
-        case 4:
-            compra.push(productos[3])
-            break;
-        case 5:
-            compra.push(productos[4])
-            break;
-        default:
-            alert("Debe ingresar un producto válido.")
-            break;
-    }
-    salida = prompt("Desea agregar otro producto al carrito? S o N").toUpperCase();
-}
-alert(`Usted agrego ${compra.length} productos a su carrito.`)
-const total = compra.reduce((acc, item) => acc + item.precio, 0)
-alert(`El total de los productos agregados es ${total} pesos`)
- */
 
-/* let contenedor = document.getElementById("contenedor");
-const papeles = [
-    { id: 1, nombre: "Papel de Celulosa Sativa Club Tradicional", precio: 350, img: "../resources/Papel-Celulosa-Sativa-Club-tradicional.png" },
-    { id: 2, nombre: "Papel OCB Blanco", precio: 220, img: "../resources/Papel-OCB-Blanco.png" },
-    { id: 3, nombre: "Papel OCB Organico", precio: 290, img: "../resources/Papel-OCB-organico.png" },
-    { id: 4, nombre: "Papel OCB Premium Negro", precio: 250, img: "../resources/Papel-OCB-Premium-Negro.jpg" },
-    { id: 5, nombre: "Papel OCB Ultimate", precio: 275, img: "../resources/Papel-OCB-ultimate_1.png" },
-]; */
-localStorage.setItem("nombre", "Gaston");
+
+
+/* localStorage.setItem("nombre", "Gaston");
 localStorage.setItem("arreglo", [1, 2, 3]);
 localStorage.setItem("valor", "true");
 let mensaje = localStorage.getItem("nombre");
-console.log(mensaje);
+console.log(mensaje); */
